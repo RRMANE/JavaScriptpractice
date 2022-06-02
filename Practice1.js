304a28293f918a33aba846846ba35c7b06ec1601
ROHIT MANE <RRMANE>
2022-06-02 19:54:00 +0530

    welcome in statement practice

---
+++ Practice1.js
@@ -0,0 +1,9 @@
+<!Doctype html>
+<html>
+    <body>
+        <h2>JavaScript Statements</h2>
+        <p>In HTML, JavaScript Statements are executed by the browser.</p>
+        <p id="demo"></p>
+        <script>document.getElementById("demo").innerHTML = "Hello Dolly.";</script>
+    </body>
+</html>
